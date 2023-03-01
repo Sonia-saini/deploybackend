2f7e3c18c7ac2ab3767cc62c7febef358ec3eabb
Sonia-saini <105914181+Sonia-saini@users.noreply.github.com>
2023-03-01 21:06:09 +0530

    adfdsf

--- configs/db.js
+++ configs/db.js
@@ -1,4 +1,4 @@
 const mongoose=require("mongoose");
-require("dotenv").config();
-const connection=mongoose.connect(process.env.URL)
+// require("dotenv").config();
+const connection=mongoose.connect(`mongodb+srv://soniasaini:soniasaini@cluster0.neg6vl9.mongodb.net/?retryWrites=true&w=majority`)
 module.exports={connection}
